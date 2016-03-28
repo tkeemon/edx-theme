d3f36aac9378f121bb2b86ef88247ddafa74f94f
Mike Bifulco <mbifulco@live.com>
2016-03-28 10:10:44 -0400

    tracking of new user registrations in cloudwall

--- static/js/gymnasium.js
+++ static/js/gymnasium.js
@@ -27,6 +27,60 @@ Gymnasium.prototype.setBackgroundColorOfElementFromImage = function (element, im
       $(element).css('background-color','rgba(' + r + ',' + g + ',' + b + ',' + a + ')');
     });
   })
-}
+};
 
+Gymnasium.prototype.RecordRegistration = function(emailAddress, firstName, lastName, cityId)
+{
+  var data = {
+    debugLink:        0,
+    first_name:       firstName,
+    last_name:        lastName,
+    email:            emailAddress,
+    location:         cityId,
+    type:             "",
+    utm_campaign:     "Registration",
+    carrot_type:      "Gymnasium Registration",
+    carrot_topic:     "GYM REG",
+    PROC:             "AWUISubmitExternalLead"
+  };
+
+  return Gymnasium.RecordCloudwallRecord(data);
+};
+
+Gymnasium.prototype.RecordCloudwallRecord = function(jsonData)
+{
+  jsonData.utm_source = "gymnasium.com";
+  jsonData.utm_medium = "web";
+  jsonData.utm_content = "not-provided";
+  jsonData.utm_term = "not-provided";
+  jsonData.agent_email = "tmashburn@aquent.com";
+  jsonData.agent_id = "1694600";
+  jsonData.agent_name = "TALENT LEAD NURTURING";
+  jsonData.carrot = "thegymnasium.com";
+  jsonData.subdomain = "cw-rc";
+  jsonData.language = "en_US";
+  jsonData.medium = "1009";
+  jsonData.referring_site = "thegymnasium.com";
+  jsonData.status = "Talent";
+  jsonData.referer = "thegymnasium.com";
+
+
+  $.ajax("http://aquent.com/application/gymnasium-lead.htm",
+    {
+      contentType: "application/json",
+      dataType: "jsonp",
+      data: jsonData
+    })
+    .done(function(event)
+    {
+      //console.log("Success!\n", event);
+    })
+    .fail(function(event, textStatus, errorThrown)
+    {
+      //console.log("Failure:\n", textStatus, "\n", errorThrown);
+    })
+    .always(function(e){
+      //console.log("always:\n", e);
+    })  ;
+}
 var Gymnasium = new Gymnasium();
