907f2ef02eea7af5080069873c4ced0da3a358ce
Mike Bifulco <mbifulco@live.com>
2016-07-29 10:01:21 -0700

    add bootstrap class names to job listings

--- static/js/gymnasium.js
+++ static/js/gymnasium.js
@@ -557,11 +557,11 @@ Gymnasium.prototype.myCustomCallback = function(response)
 
   for (var i = 0; i < jobs.length; i ++)
   {
-    var li = '<li>';
+    var li = '<li class="row">';
     li += '<a href=https://aquent.com/find-work/' + jobs[i].jobId + '>';
     li += '<div class="job-post">';
-    li += '<b class="job-title">' + jobs[i].title + '</b>';
-    li += '<em class="job-market"> ' + jobs[i].marketId + '</em>';
+    li += '<b class="job-title col-md-8">' + jobs[i].title + '</b>';
+    li += '<em class="job-market col-md-4 text-right"> ' + jobs[i].marketId + '</em>';
     li += '</div>';
     li += '</a>';
     li += '</li>';
