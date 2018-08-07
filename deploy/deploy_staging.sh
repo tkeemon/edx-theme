3801b46e26a48147204a15f4786c06ae34530201
TJ Keemon <keems21@gmail.com>
2018-08-07 15:16:51 -0400

    first attempt at circle 2.0 fix
    move circleci config file to correct place
    
    fix to config
    
    add "sudo" to pip install command
    
    remove "deactivate" from deploy step
    
    deploy correct branch to staging
    
    specify repo url to cirleci
    
    correct repo variable
    
    remove repo URL var

--- deploy/deploy_staging.sh
+++ deploy/deploy_staging.sh
@@ -4,7 +4,7 @@ cd ~
 git clone https://github.com/appsembler/configuration 
 cd configuration 
 git checkout appsembler/eucalyptus/master 
-pip install -r requirements.txt 
+sudo pip install -r requirements.txt 
 cd playbooks  
 
 #link edx_ansible roles and libraries
@@ -12,5 +12,5 @@ ln -s ~/configuration/playbooks/roles ~/edx-theme/deploy/roles
 ln -s ~/configuration/playbooks/library ~/edx-theme/deploy/library
 
 #deploy
-ansible-playbook -i ~/inventory --user $CIRCLE_USER ~/edx-theme/deploy/aquent_deploy.yml
+ansible-playbook -i ~/inventory --user $CIRCLE_USER -e THEME_BRANCH=$CIRCLE_BRANCH ~/edx-theme/deploy/aquent_deploy.yml
 
